41f4bd24bf0f3ae31764b0540a94395aacdd7913
fokhruli <fokhrul.rmedu@gmail.com>
2021-04-21 03:19:52 +0600

    transformer2

--- attention/main.py
+++ attention/main.py
@@ -16,4 +16,26 @@ class SelfAttention(nn.Module):
 
     def forward(self, values, keys, query, mask):
         N = query.shape[0]
-        
+        values_len, keys_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+        # split data into multiple head
+        values = values.reshape(N, values_len, self.head, self.head_dim)
+        keys = keys.reshape(N, keys_len, self.head, self.head_dim)
+        query = query.reshape(N, query_len, self.head, self.head_dim)
+
+        energy = torch.einsum("nqhd, nkhd-->nhqk", [query, keys])
+        # query shape: (N, query_len, head, head_dim)
+        # keys shape: (N, keys_len, head, head_dim)
+        # energy shape: (N, head, quary_len, keys_len)
+        if mask is not None:
+            energy = energy.masked_fill(mask = 0, float("-1e20"))
+        attention = torch.softmax(energy/(self.embed_size **(1/2)), dim = 3)
+
+        out = torch.einsum("nhql, nlhd-->nqhd", [attention, values]).reshape(
+            N, query_len, self.head*self.head_dim
+        )
+
+        # attention shape: (N, heads, query_len, keys_len)
+        # values shape: (N, values_len, head, head_dim)
+        # out shape : (N, query_len, head, head_dim) --> keys_len == values_len
+        out = self.fc_out(out)
+        return out
