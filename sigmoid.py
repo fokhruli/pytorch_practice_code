c55b902a7ea95eee1dbffb2a5831d3a027c5eadf
Md Fokhrul Islam <fokhrul.rmedu@gmail.com>
2021-06-03 16:03:06 +0600

    Create sigmoid.py

---
+++ sigmoid.py
@@ -0,0 +1,16 @@
+# Import matplotlib, numpy and math
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+  
+x = np.linspace(-10, 10, 100)
+z = 1/(1 + np.exp(-x))
+  
+plt.plot(x, z)
+plt.axis('off')
+##plt.xlabel("x")
+#plt.ylabel("Sigmoid(X)")
+
+plt.tight_layout()
+
+plt.savefig('sigmoid.png')
